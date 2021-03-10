34df14a4dbdffed9672bcd4e552c79be45941b7c
Liv <lopez.olivia17@outlook.com>
2021-03-10 15:13:19 -0600

    Added schedule array and started creating timeblock elements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,58 @@
+// Schedule Array containing times 
+
+var schedule = [
+  {
+    hour: '9';,
+    meridiem: 'am';
+  };
+  {
+    hour: '10';,
+    meridiem: 'am';
+  };
+  {
+    hour: '11';,
+    meridiem: 'am';
+  }
+  {
+    hour: '12';,
+    meridiem: 'pm';
+  }
+  {
+    hour: '1';,
+    meridiem: 'am';
+  }
+  {
+    hour: '2';,
+    meridiem: 'pm';
+  }
+  {
+    hour: '3';,
+    meridiem: 'pm';
+  }
+  {
+    hour: '4';,
+    meridiem: 'pm';
+  }
+  {
+    hour: '5';,
+    meridiem: 'pm';
+  }
+];
+
+
+
 // Current Date at top of page 
 var CurrentDate = moment ();
 $("#currentDay"). text(CurrentDate.format("dddd MMMM Do, YYYY"));
 
-// Timeblocks 
-  // Select the existing DOM element, <div class="container">, and assign to a new variable
+// Select the existing DOM element <div class="container">
+var containerEl = $('#container');
+
+// Create/add a forum, textarea, and button to create the individual timeblocks
+var forumEl = $('<forum>');
+var timeEl = $('<div>');
+var textBlockEl = $('<div>');
+var textAreaEl = $('<Textarea>');
+var saveBtnEl = $('<button>')
+
+
