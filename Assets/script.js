5d109a3eb1204421d6e1766d2b968e57dfc0a997
Liv <lopez.olivia17@outlook.com>
2021-03-10 09:24:08 -0600

    Added moment JS feature to the top of the page

--- Assets/script.js
+++
@@ -1,3 +0,0 @@
-// Current Date at top of page 
-var CurrentDate = moment ();
-$("#currentDay"). text(CurrentDate.format("MMM Do, YYYY"));
---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+// Current Date at top of page 
+var CurrentDate = moment ();
+$("#currentDay"). text(CurrentDate.format("dddd MMMM Do, YYYY"));
+
+// Timeblocks 
+  // Select the existing DOM element, <div class="container">, and assign to a new variable
